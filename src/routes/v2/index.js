bd95ef22911d6a425faa1c71367b805027c1fbbe
shreypilot <shreypilot28@gmail.com>
2024-01-13 21:31:24 +0530

    added encryption in signup

--- src/routes/v2/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/info',(req,res) =>{
-    return res.json({msg:'coming from v2 api'});
-});
-
-module.exports = router;
