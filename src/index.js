c59fe5b978f7d8d44b74ffa9ff6b35a7bda5e794
shreypilot <shreypilot28@gmail.com>
2024-01-13 21:02:53 +0530

    added new user signup api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
-const { ServerConfig,Logger } = require('./config');
+const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-//see later how it print error statement
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const { InfoController } = require('../../controllers');
 
+const userRouter = require('./user-routes');
 const router = express.Router();
 
-router.get('/info', InfoController.info);// when info routes call then infocontroller will called
+router.get('/info', InfoController.info);
+
+router.use('/signup', userRouter)
 
 module.exports = router;
