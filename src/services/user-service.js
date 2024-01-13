e21386d507c1c1806476eb5654113b448ff8a5b1
shreypilot <shreypilot28@gmail.com>
2024-01-13 21:39:50 +0530

    added sign in with jwt creation

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 const { UserRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
 const userRepo = new UserRepository();
+const { Auth } = require('../utils/common');
 
 async function create(data) {
     try {
@@ -19,7 +20,27 @@ async function create(data) {
         throw new AppError('Cannot create a new user object', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+async function signin(data) {
+    try {
+        const user = await userRepo.getUserByEmail(data.email);
+        if(!user) {
+            throw new AppError('No user found for the given email', StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch = Auth.checkPassword(data.password, user.password);
+        console.log("password match", passwordMatch)
+        if(!passwordMatch) {
+            throw new AppError('Invalid password', StatusCodes.BAD_REQUEST);
+        }
+        const jwt = Auth.createToken({id: user.id, email: user.email});
+        return jwt;
+    } catch(error) {
+        if(error instanceof AppError) throw error;
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
 
 module.exports = {
-    create
+    create,
+    signin
 }
