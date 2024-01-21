e1fa32fa9fbc3861d4e52d6aa2ab063484cf7822
shreypilot <shreypilot28@gmail.com>
2024-01-21 13:58:43 +0530

    added modesl for many to many assoc between roles and users

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const limiter = rateLimit({
     windowMs:2*60*1000,//2min
-    max:3,//limit each ip to 2 requests per window here per 15 min
+    max:30,//limit each ip to 2 requests per window here per 15 min
 })
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(limiter)
 
-
+app.use('/api', apiRoutes);
 app.use('/flightsService', createProxyMiddleware({ target: ServerConfig.FLIGHT_SERVICE, changeOrigin: true, pathRewrite: {'^/flightsService' : '/'} }));
 app.use('/bookingService', createProxyMiddleware({ target: ServerConfig.BOOKING_SERVICE, changeOrigin: true }));
-app.use('/api', apiRoutes);
+
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
